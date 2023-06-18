64e7b15275bae40636727e041fed39100c81280d
geithamum <136993352+geithamum@users.noreply.github.com>
2023-06-18 11:18:25 -0600

    Added main script and GUI script

---
+++ FileOrganizerGui.py
@@ -0,0 +1,48 @@
+import tkinter as tk
+from tkinter import filedialog
+import FileOrganizerMainScript
+
+def browse_source_button():
+    global source_directory
+    source_directory = filedialog.askdirectory()
+    source_directory_label.config(text=source_directory)
+
+def browse_destination_button():
+    global destination_directory
+    destination_directory = filedialog.askdirectory()
+    destination_directory_label.config(text=destination_directory)
+
+def organize_button():
+    if source_directory and destination_directory:
+        FileOrganizerMainScript.organize_files(source_directory, destination_directory)
+        status_label.config(text="Files organized successfully!")
+    else:
+        status_label.config(text="Please select both source and destination directories.")
+
+# Create the GUI window
+window = tk.Tk()
+window.title("File Organization Tool")
+window.geometry("400x250")
+
+# Create the browse button and label for source directory selection
+browse_source_button = tk.Button(window, text="Select Source Directory", command=browse_source_button)
+browse_source_button.pack(pady=10)
+source_directory_label = tk.Label(window, text="No source directory selected")
+source_directory_label.pack()
+
+# Create the browse button and label for destination directory selection
+browse_destination_button = tk.Button(window, text="Select Destination Directory", command=browse_destination_button)
+browse_destination_button.pack(pady=10)
+destination_directory_label = tk.Label(window, text="No destination directory selected")
+destination_directory_label.pack()
+
+# Create the organize button
+organize_button = tk.Button(window, text="Organize Files", command=organize_button)
+organize_button.pack(pady=10)
+
+# Create the status label
+status_label = tk.Label(window, text="")
+status_label.pack(pady=10)
+
+# Start the GUI event loop
+window.mainloop()
---
+++ FileOrganizerMainScript.py
@@ -0,0 +1,33 @@
+import os
+import shutil
+
+
+def organize_files(source_directory, destination_directory):
+    # Get a list of files in the source directory
+    files = os.listdir(source_directory)
+    print('files listed')
+
+
+    for file in files:
+        if os.path.isfile(os.path.join(source_directory, file)):  # Only process files, not directories
+            file_extension = os.path.splitext(file)[1]  # Get the file extension
+            print('extensions split')
+            # Define destination folders based on file types
+            if file_extension == ".txt":
+                destination_folder = "TextFiles"
+            elif file_extension == ".jpg" or file_extension == ".png":
+                destination_folder = "Images"
+            elif file_extension == ".docx" or file_extension == ".xlsx" or file_extension == ".pptx":
+                destination_folder = "Documents"
+
+
+            else:
+                destination_folder = "Miscellaneous"
+
+            # Create the destination folder if it doesn't exist
+            if not os.path.exists(destination_folder):
+                os.makedirs(destination_folder)
+
+            # Move the file to the destination folder
+            shutil.move(os.path.join(source_directory, file),
+                        os.path.join(destination_directory, destination_folder, file))
